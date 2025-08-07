76ccf86b70af25c036c214c9d04da1da684c1786
rkwh2005 <raphaelkwh@gmail.com>
2025-08-07 16:27:30 +0800

    Update MOT_TMS_pipeline_config.m

--- functions/config/MOT_TMS_pipeline_config.m
+++ functions/config/MOT_TMS_pipeline_config.m
@@ -29,13 +29,20 @@ config.input.tms_conditions_table = table( ...
                                     'VariableNames', {'Condition','Types'} ...
                                 );
 
+% Duration check config
+config.durationCheck.expectedDurations = struct( ...
+    'cTBS', struct('expected', 40, 'tolerance', 1), ...
+    'Vertex', struct('expected', 40, 'tolerance', 1), ...
+    'iTBS', struct('expected', 188, 'tolerance', 1) ...
+);
+
 config.input.tms_condspec_file = 'Tim_condspec.csv';
 
 %% Utility script settings (optional)
-config.run_organizeData = false; % Create simplified Directory structure (necessary for first run)
 config.utils.run_validSessWide2Long = true; % Set to true to run validSessWide2Long.m
-config.utils.run_manualReplaceFile = true; % Set to true to run manualReplaceFile.m
+config.utils.run_manualReplaceFile = true;  % Set to true to run manualReplaceFile.m
 config.utils.run_insmarker_equivalence = true; % Set to true to run insmarker_equivalence.m
+config.utils.run_checkDurationMatch = true; % New: Set to true to run checkDurationMatch.m
 
 % Parameters for validSessWide2Long.m
 config.utils.validSess.wide_file = 'MOT_optional_valid_session_list_wide.csv';
@@ -48,8 +55,9 @@ config.utils.manualReplace.replacement_map_file = 'MOT_optional_overwriting_vali
 config.utils.insmarker_equivalence.markers = ["iTBS", "cTBS"];
 
 %% Output file names
-config.output.extracted_paths = 'step_1_all_extracted_path.csv';
-config.output.valid_files = 'step_2_all_valid_files.csv';
-config.output.all_files = 'step_3_all_files_with_coord.csv';
-config.output.tms_quality_csv = 'Final_TMS_Quality.csv'; % New output for quality results
-
+config.output.extracted_paths = 'step_1_all_extracted_path.mat';
+config.output.valid_files = 'step_2_all_valid_files.mat';
+config.output.all_files = 'step_3_all_files_with_coord.mat';
+config.output.tms_quality_raw = 'TMS_Quality_Raw.mat'; 
+config.output.tms_quality_sum = 'TMS_Quality_Summary.mat'; % New output for quality results
+config.dirs.output_save_path = fullfile('data', 'organized', config.dataset_name);
